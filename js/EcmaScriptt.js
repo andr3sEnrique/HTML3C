e432af90ca1ec135e61171083bae549479ed027c
andr3sEnrique <20213tn080@utez.edu.mx>
2022-06-17 10:45:31 -0500

    nuevos codigos

---
+++ js/EcmaScriptt.js
@@ -0,0 +1,13 @@
+function sumaNormal(a, b) {
+	return a + b;
+}
+console.log(sumaNormal(2, 5));
+
+let sumaArrow = (a, b) => a + b;
+console.log(sumaArrow(5, 8));
+
+let sumaArrowSP = () => {
+	let suma = 10 + 5;
+	return suma;
+};
+console.log(sumaArrowSP());
